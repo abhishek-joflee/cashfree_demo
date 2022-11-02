84ce5bcb551d5e33c5b3108f863344b7a1bae353
Abhishek Patil <abhishek.patil@joflee.com>
2022-11-02 14:25:46 +0530

    flutter setup added

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cferrorresponse/cferrorresponse.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfpayment/cfdropcheckoutpayment.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfpaymentcomponents/cfpaymentcomponent.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfpaymentgateway/cfpaymentgatewayservice.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cfsession/cfsession.dart';
+import 'package:flutter_cashfree_pg_sdk/api/cftheme/cftheme.dart';
+import 'package:flutter_cashfree_pg_sdk/utils/cfenums.dart';
+import 'package:flutter_cashfree_pg_sdk/utils/cfexceptions.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var cfPaymentGatewayService = CFPaymentGatewayService();
+
+  @override
+  void initState() {
+    super.initState();
+    cfPaymentGatewayService.setCallback(verifyPayment, onError);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            children: [TextButton(onPressed: pay, child: const Text("Pay"))],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  void verifyPayment(String orderId) {
+    print("Verify Payment");
+  }
+
+  void onError(CFErrorResponse errorResponse, String orderId) {
+    print("Error while making payment");
+  }
+
+  String orderId = "Order0001";
+  String orderToken =
+      "7M9JCN4MzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye.BD0nI4ATOkRDNzQmMyYzM2IiOiQHbhN3XiwCN4QDM3kTO2YTM6ICc4VmIsIiUOlkI6ISej5WZyJXdDJXZkJ3biwiIxIiOiQnb19WbBJXZkJ3biwiIxADMwIXZkJ3TiojIklkclRmcvJye.hDp-g1JFGxgIElFAA80LdMo6W_KfTutsrC_SO8jxT0VLK0HFumejXGJzUMwM--qBhF";
+  CFEnvironment environment = CFEnvironment.SANDBOX;
+
+  CFSession? createSession() {
+    try {
+      var session = CFSessionBuilder()
+          .setEnvironment(environment)
+          .setOrderId(orderId)
+          .setOrderToken(orderToken)
+          .build();
+      return session;
+    } on CFException catch (e) {
+      print(e.message);
+    }
+    return null;
+  }
+
+  pay() async {
+    try {
+      var session = createSession();
+      List<CFPaymentModes> components = <CFPaymentModes>[];
+      var paymentComponent =
+          CFPaymentComponentBuilder().setComponents(components).build();
+
+      var theme = CFThemeBuilder()
+          .setNavigationBarBackgroundColorColor("#FF0000")
+          .setPrimaryFont("Menlo")
+          .setSecondaryFont("Futura")
+          .build();
+
+      var cfDropCheckoutPayment = CFDropCheckoutPaymentBuilder()
+          .setSession(session!)
+          .setPaymentComponent(paymentComponent)
+          .setTheme(theme)
+          .build();
+
+      cfPaymentGatewayService.doPayment(cfDropCheckoutPayment);
+    } on CFException catch (e) {
+      print(e.message);
+    }
   }
 }
