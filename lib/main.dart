96d8134ac7e45df948a3360136525c85e0c6757b
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-11-25 11:04:46 +0530

    contiueing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(
       const MaterialApp(
@@ -19,6 +23,87 @@ class MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  Future<void> _launchUrl(url) async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Future<bool> listenForPayment(String orderID) async {
+    final nav = Navigator.of(context);
+    var headers = {
+      'Accept': 'application/json',
+      'x-api-version': '2022-01-01',
+      'x-client-id': '1950953d7e9231a3f1df2c9a69590591',
+      'x-client-secret': '21670a53a5bd9568b36cacfd8b7db6cce9446544'
+    };
+    var request = http.Request('GET',
+        Uri.parse('https://sandbox.cashfree.com/pg/orders/$orderID/payments'));
+
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      final data = await response.stream.bytesToString();
+      final paymentStatusReceived = json.decode(data) != [];
+      if (paymentStatusReceived) {
+        nav.push(
+          MaterialPageRoute(
+            builder: (context) => const Text('Payment status received'),
+          ),
+        );
+        return true;
+      }
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
+    return false;
+  }
+
+  void doPayment() async {
+    var headers = {
+      'Content-Type': 'application/json',
+      'x-api-version': '2022-01-01',
+      'Accept': 'application/json'
+    };
+    var request = http.Request(
+        'POST', Uri.parse('https://sandbox.cashfree.com/pg/orders/pay'));
+    request.body = json.encode({
+      "order_token": "z73NUOVEx0Ur2aNFCfqe",
+      "payment_method": {
+        "card": {
+          "channel": "link",
+          "card_number": "4706131211212123",
+          "card_holder_name": "Abhishek Patil",
+          "card_expiry_mm": "07",
+          "card_expiry_yy": "23",
+          "card_cvv": "123"
+        }
+      },
+      "save_instrument": true
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      final data = await response.stream.bytesToString();
+      final paymentURL = json.decode(data)['data']['url'];
+      print(data);
+      print(paymentURL);
+      _launchUrl(paymentURL);
+      bool isDone = false;
+      while (!isDone) {
+        isDone =
+            await listenForPayment('order_1950952I1gDcqXCAJTJdHuwQqq03TZd5a');
+      }
+    } else {
+      print(response.reasonPhrase);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,6 +116,11 @@ class MyAppState extends State<MyApp> {
             Text('hi'),
           ],
         ),
+        floatingActionButton: FloatingActionButton.extended(
+          label: const Text('Make Payment'),
+          onPressed: doPayment,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
