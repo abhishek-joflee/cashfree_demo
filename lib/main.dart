49bad341b1ded7ac05394feed6e24fa5484b28f3
ABHISHEK PATIL <abhishek.patil@joflee.com>
2022-11-25 10:25:41 +0530

    improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:cashfree_pg/cashfree_pg.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: MyApp()));
+void main() => runApp(
+      const MaterialApp(
+        home: MyApp(),
+      ),
+    );
+
+String orderId = "order_1950952I1bUkaYX4gZiBiPhnfNwq7ylca";
+String stage = "TEST";
+String orderAmount = "15.00";
+String tokenData =
+    "b89JCN4MzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye.lwQfikzMykzNkdTY0ADOzYjI6ICdsF2cfJCLxYDM0QTOxcjNxojIwhXZiwiIS5USiojI5NmblJnc1NkclRmcvJCL1EjOiQnb19WbBJXZkJ3biwiIhNGb5dTc35kZuhGUpJUaadGNYlVYrVlYxkkM1kDM1kTMfJXZkJ3biojIklkclRmcvJye.srx9vItVYvjGD4MdL3mZloQNpN4CKvtN0-jMgII2x6yKg5ha7xA18qQFxUCO3K8knb";
+String customerName = "Abhishek";
+String orderNote = "Order_Note";
+String orderCurrency = "INR";
+String appId = "1950953d7e9231a3f1df2c9a69590591";
+String customerPhone = "9638306280";
+String customerEmail = "abhishek.p@codeshastra.com";
+String notifyUrl = "https://test.gocashfree.com/notify";
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => _MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> {
   var _selectedApp;
 
   @override
@@ -94,19 +113,6 @@ class _MyAppState extends State<MyApp> {
 
   // WEB Intent
   makePayment() {
-    //Replace with actual values
-    String orderId = "ORDER_ID";
-    String stage = "PROD";
-    String orderAmount = "ORDER_AMOUNT";
-    String tokenData = "TOKEN_DATA";
-    String customerName = "Customer Name";
-    String orderNote = "Order_Note";
-    String orderCurrency = "INR";
-    String appId = "APP_ID";
-    String customerPhone = "Customer Phone";
-    String customerEmail = "sample@gmail.com";
-    String notifyUrl = "https://test.gocashfree.com/notify";
-
     Map<String, dynamic> inputParams = {
       "orderId": orderId,
       "orderAmount": orderAmount,
@@ -130,19 +136,6 @@ class _MyAppState extends State<MyApp> {
 
   // SEAMLESS - CARD
   Future<void> seamlessCardPayment() async {
-    String orderId = "order_1950952I1YUlOHRNZPpTVVNZMMJx0qeW7";
-    String stage = "TEST";
-    String orderAmount = "15";
-    String tokenData =
-        "px9JCN4MzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye.vsQfiQDMjJTY0IGN0ADOzYjI6ICdsF2cfJCLwgTNyQTOxcjNxojIwhXZiwiIS5USiojI5NmblJnc1NkclRmcvJCL1EjOiQnb19WbBJXZkJ3biwiI3cVZxBDeK1UTa5kVWRFcQplTSh0TsVVWxkkM1kDM1kTMfJXZkJ3biojIklkclRmcvJye.khY9qdJzWklh1tP2CHkgRKYqELY6SPs-vju3AnqVv0Hp6eDDFDxarDuzCMgxpQNWcX";
-    String customerName = "Customer Name";
-    String orderNote = "Order_Note";
-    String orderCurrency = "INR";
-    String appId = "1950953d7e9231a3f1df2c9a69590591";
-    String customerPhone = "9638306280";
-    String customerEmail = "sample@gmail.com";
-    String notifyUrl = "https://test.gocashfree.com/notify";
-
     Map<String, dynamic> inputParams = {
       "orderId": orderId,
       "orderAmount": orderAmount,
@@ -174,18 +167,6 @@ class _MyAppState extends State<MyApp> {
 
   // SEAMLESS - NETBANKING
   Future<void> seamlessNetbankingPayment() async {
-    String orderId = "ORDER_ID";
-    String stage = "PROD";
-    String orderAmount = "ORDER_AMOUNT";
-    String tokenData = "TOKEN_DATA";
-    String customerName = "Customer Name";
-    String orderNote = "Order_Note";
-    String orderCurrency = "INR";
-    String appId = "APP_ID";
-    String customerPhone = "Customer Phone";
-    String customerEmail = "sample@gmail.com";
-    String notifyUrl = "https://test.gocashfree.com/notify";
-
     Map<String, dynamic> inputParams = {
       "orderId": orderId,
       "orderAmount": orderAmount,
@@ -214,18 +195,6 @@ class _MyAppState extends State<MyApp> {
 
   // SEAMLESS - WALLET
   Future<void> seamlessWalletPayment() async {
-    String orderId = "ORDER_ID";
-    String stage = "PROD";
-    String orderAmount = "ORDER_AMOUNT";
-    String tokenData = "TOKEN_DATA";
-    String customerName = "Customer Name";
-    String orderNote = "Order_Note";
-    String orderCurrency = "INR";
-    String appId = "APP_ID";
-    String customerPhone = "Customer Phone";
-    String customerEmail = "sample@gmail.com";
-    String notifyUrl = "https://test.gocashfree.com/notify";
-
     Map<String, dynamic> inputParams = {
       "orderId": orderId,
       "orderAmount": orderAmount,
@@ -293,18 +262,6 @@ class _MyAppState extends State<MyApp> {
 
   // SEAMLESS - Paypal
   Future<void> seamlessPayPalPayment() async {
-    String orderId = "ORDER_ID";
-    String stage = "PROD";
-    String orderAmount = "ORDER_AMOUNT";
-    String tokenData = "TOKEN_DATA";
-    String customerName = "Customer Name";
-    String orderNote = "Order_Note";
-    String orderCurrency = "INR";
-    String appId = "APP_ID";
-    String customerPhone = "Customer Phone";
-    String customerEmail = "sample@gmail.com";
-    String notifyUrl = "https://test.gocashfree.com/notify";
-
     Map<String, dynamic> inputParams = {
       "orderId": orderId,
       "orderAmount": orderAmount,
@@ -331,20 +288,6 @@ class _MyAppState extends State<MyApp> {
 
   // UPI Intent
   Future<void> makeUpiPayment() async {
-    //Replace with actual values
-    String orderId = "order_1950952I1YpPbFLzYefkl19G6dKmj8Qhs";
-    String stage = "TEST";
-    String orderAmount = "15";
-    String tokenData =
-        "dL9JCN4MzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye.N7QfiE2N1MmNmZTN0ADOzYjI6ICdsF2cfJCL3YzNyQTOxcjNxojIwhXZiwiIS5USiojI5NmblJnc1NkclRmcvJCL1EjOiQnb19WbBJXZkJ3biwiIzhWU4oWbLRmNHlTMstmZllleMZkYQBXWxkkM1kDM1kTMfJXZkJ3biojIklkclRmcvJye.NWT3eOh2Pjlig06ee85im8t4gbEvKk32Zg8GDxorOgG8bQcYGjB3dNFza26E8hag7a";
-    String customerName = "Customer Name";
-    String orderNote = "Order_Note";
-    String orderCurrency = "INR";
-    String appId = "1950953d7e9231a3f1df2c9a69590591";
-    String customerPhone = "Customer Phone";
-    String customerEmail = "sample@gmail.com";
-    String notifyUrl = "https://test.gocashfree.com/notify";
-
     Map<String, dynamic> inputParams = {
       "orderId": orderId,
       "orderAmount": orderAmount,
@@ -368,20 +311,6 @@ class _MyAppState extends State<MyApp> {
 
   // SEAMLESS UPI Intent
   Future<void> seamlessUPIIntent() async {
-    //Replace with actual values
-    String orderId = "order_1950952I1YpPbFLzYefkl19G6dKmj8Qhs";
-    String stage = "TEST";
-    String orderAmount = "15";
-    String tokenData =
-        "9N9JCN4MzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye.CHQfiYWYhZmMjZzM0ADOzYjI6ICdsF2cfJCLyUjMyQTOxcjNxojIwhXZiwiIS5USiojI5NmblJnc1NkclRmcvJCL1EjOiQnb19WbBJXZkJ3biwiI2pmN5JUQ4JXV0EGeG5UezQnUMpmN4oGWxkkM1kDM1kTMfJXZkJ3biojIklkclRmcvJye.bmpWVAlEHcMwvxTFgneKB4uM5Cf8heL5yGqKIN-EayTTxXkLzNRqaCXvWpfegIpueL";
-    String customerName = "Customer Name";
-    String orderNote = "Order_Note";
-    String orderCurrency = "INR";
-    String appId = "1950953d7e9231a3f1df2c9a69590591";
-    String customerPhone = "Customer Phone";
-    String customerEmail = "sample@gmail.com";
-    String notifyUrl = "https://test.gocashfree.com/notify";
-
     Map<String, dynamic> inputParams = {
       "orderId": orderId,
       "orderAmount": orderAmount,
